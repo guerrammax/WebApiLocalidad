ef9ff554b2e618ce48d7ffe1c201ad89e3ade5dc
guerrammax <maximino_guerra@hotmail.com>
2019-02-21 12:26:39 -0700

    crud Pais

--- Controllers/PaisController.cs
+++ Controllers/PaisController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApiLocalidad.Models;
 
 namespace WebApiLocalidad.Controllers
@@ -15,8 +16,62 @@ namespace WebApiLocalidad.Controllers
             this.context = context;
         }
 
+        [HttpGet]
         public IEnumerable<Pais> Get(){
+
             return context.Paises.ToList();
         }
+
+        [HttpGet("{id}", Name="paisCreado")]
+        public IActionResult GetById(int id){
+            
+            var pais = context.Paises.FirstOrDefault(x=> x.Id == id);
+
+            if (pais == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pais);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Pais pais){
+            if (ModelState.IsValid)
+            {
+                context.Paises.Add(pais);
+                context.SaveChanges();
+
+                return new CreatedAtRouteResult("paisCreado", new {id = pais.Id}, pais);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] Pais pais, int id){
+            if (pais.Id != id)
+            {
+                return BadRequest();
+                
+            }
+            context.Entry(pais).State = EntityState.Modified;
+            context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete( int id){
+
+           var pais = context.Paises.FirstOrDefault(x => x.Id ==id);
+
+            if (pais == null)
+            {
+                return NotFound();            
+            }
+            context.Paises.Remove(pais);
+            context.SaveChanges();
+            return Ok(pais);
+        }
+        
     }
 }
--- Models/Pais.cs
+++ Models/Pais.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApiLocalidad.Models
 {
     public class Pais
     {
         public int Id { get; set; }
+        [StringLength(30)]
         public string Nombre { get; set; }
     }
 }
