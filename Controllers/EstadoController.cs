2551beb00e2975b2c21ef7128bdd40741bbe7fde
guerrammax <maximino_guerra@hotmail.com>
2019-02-21 15:21:35 -0700

    crud estado

--- Controllers/EstadoController.cs
+++ Controllers/EstadoController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApiLocalidad.Models;
 
 namespace WebApiLocalidad.Controllers
@@ -24,6 +25,59 @@ namespace WebApiLocalidad.Controllers
             //Retorna los estasdos del pais id
             return context.Estados.Where(x => x.PaisId == PaisId).ToList();
 
+        }
+
+        [HttpGet("{id}", Name="estadoById")]
+        public IActionResult GetById(int id){
+            var pais = context.Estados.FirstOrDefault(x=>x.Id == id);
+
+            if (pais==null)
+            {
+                return NotFound();
+            }
+
+            return new ObjectResult(pais);
+
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Estado estado, int idPais)
+        {
+            estado.PaisId =idPais;
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            context.Estados.Add(estado);
+            context.SaveChanges();
+
+            return new CreatedAtRouteResult("provinciaById", new {id = estado.Id}, estado);
+       
+        }
+        
+        [HttpPut("{id}")]
+        public IActionResult Update([FromBody] Estado estado, int id){
+            if (estado.Id != id)
+            {
+                return BadRequest();
+            }
+
+            context.Entry(estado).State = EntityState.Modified;
+            context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id){
+            var estado = context.Estados.FirstOrDefault(x=>x.Id == id);
+            if (estado == null)
+            {
+                return NotFound();
+            }
+            context.Estados.Remove(estado);
+            context.SaveChanges();
+            return Ok(estado);
         }
     }
 }
